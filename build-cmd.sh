bf82f5eb90e998ecbbf7276baf35a26e347e2854
AiraYumi <aira.youme@airanyumi.net>
2025-02-27 04:47:58 +0000

    add submodule sdl3

--- build-cmd.sh
+++ build-cmd.sh
@@ -156,5 +156,121 @@ case "$AUTOBUILD_PLATFORM" in
 esac
 popd
 
+# SDL3
+pushd "$TOP/SDL3"
+
+case "$AUTOBUILD_PLATFORM" in
+    windows*)
+        load_vsvars
+
+        mkdir -p "$stage/include/SDL3"
+        mkdir -p "$stage/lib/debug"
+        mkdir -p "$stage/lib/release"
+
+        mkdir -p "build_debug"
+        pushd "build_debug"
+            cmake .. -G "Ninja" -DCMAKE_INSTALL_PREFIX=$(cygpath -m $stage)/debug
+
+            cmake --build . --config Debug
+            cmake --install . --config Debug
+
+            # conditionally run unit tests
+            if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
+                ctest -C Debug
+            fi
+
+            cp $stage/debug/bin/*.dll $stage/lib/debug/
+            cp $stage/debug/lib/*.lib $stage/lib/debug/
+        popd
+
+        mkdir -p "build_release"
+        pushd "build_release"
+            cmake .. -G "Ninja" -DCMAKE_INSTALL_PREFIX=$(cygpath -m $stage)/release
+
+            cmake --build . --config Release
+            cmake --install . --config Release
+
+            # conditionally run unit tests
+            if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
+                ctest -C Release
+            fi
+
+            cp $stage/release/bin/*.dll $stage/lib/release/
+            cp $stage/release/lib/*.lib $stage/lib/release/
+            cp $stage/release/include/SDL3/*.h $stage/include/SDL3/
+        popd
+    ;;
+    darwin*)
+        export MACOSX_DEPLOYMENT_TARGET="$LL_BUILD_DARWIN_DEPLOY_TARGET"
+
+        for arch in x86_64 arm64 ; do
+            ARCH_ARGS="-arch $arch"
+            opts="${TARGET_OPTS:-$ARCH_ARGS $LL_BUILD_RELEASE}"
+            cc_opts="$(remove_cxxstd $opts)"
+            ld_opts="$ARCH_ARGS"
+
+            mkdir -p "build_$arch"
+            pushd "build_$arch"
+                CFLAGS="$cc_opts" \
+                CXXFLAGS="$opts" \
+                LDFLAGS="$ld_opts" \
+                cmake .. -GNinja -DCMAKE_BUILD_TYPE="Release" \
+                    -DCMAKE_C_FLAGS="$cc_opts" \
+                    -DCMAKE_CXX_FLAGS="$opts" \
+                    -DCMAKE_OSX_ARCHITECTURES:STRING="$arch" \
+                    -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET} \
+                    -DCMAKE_MACOSX_RPATH=YES \
+                    -DCMAKE_INSTALL_PREFIX="$stage" \
+                    -DCMAKE_INSTALL_LIBDIR="$stage/lib/release/$arch"
+
+                cmake --build . --config Release
+                cmake --install . --config Release
+            popd
+        done
+
+
+        # create universal libraries
+        lipo -create -output ${stage}/lib/release/libSDL3.0.dylib ${stage}/lib/release/x86_64/libSDL3.0.dylib ${stage}/lib/release/arm64/libSDL3.0.dylib
+        lipo -create -output ${stage}/lib/release/libSDL3.dylib ${stage}/lib/release/x86_64/libSDL3.dylib ${stage}/lib/release/arm64/libSDL3.dylib
+        lipo -create -output ${stage}/lib/release/libSDL3_test.a ${stage}/lib/release/x86_64/libSDL3_test.a ${stage}/lib/release/arm64/libSDL3_test.a
+
+        pushd "${stage}/lib/release"
+            install_name_tool -id "@rpath/libSDL3.dylib" "libSDL3.dylib"
+            dsymutil libSDL3.dylib
+            strip -x -S libSDL3.dylib
+        popd
+        ;;
+    linux*)
+        # Default target per autobuild build --address-size
+        opts="${TARGET_OPTS:--m$AUTOBUILD_ADDRSIZE $LL_BUILD_RELEASE}"
+
+        mkdir -p "$stage/include/SDL3"
+        mkdir -p "$stage/lib/release"
+
+        PREFIX_RELEASE="$stage/temp_release"
+        mkdir -p $PREFIX_RELEASE
+
+        mkdir -p "build_release"
+        pushd "build_release"
+            cmake .. -GNinja -DCMAKE_BUILD_TYPE="Release" \
+                -DCMAKE_C_FLAGS="$(remove_cxxstd $opts)" \
+                -DCMAKE_CXX_FLAGS="$opts" \
+                -DCMAKE_INSTALL_PREFIX=$PREFIX_RELEASE
+
+            cmake --build . --config Release
+            cmake --install . --config Release
+        popd
+
+        cp -a $PREFIX_RELEASE/include/SDL3/*.* $stage/include/SDL3
+        cp -a $PREFIX_RELEASE/lib/*.so* $stage/lib/release
+        cp -a $PREFIX_RELEASE/lib/lib*.a $stage/lib/release
+    ;;
+
+    *)
+        exit -1
+    ;;
+esac
+popd
+
 mkdir -p "$stage/LICENSES"
 cp "$TOP/$SDL_SOURCE_DIR/LICENSE.txt" "$stage/LICENSES/SDL2.txt"
